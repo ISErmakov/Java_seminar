0aa5ffdcb19dda94650276571e4e07c20bb117f5
Ermakovis <ermakovis_dev@gmail.com>
2023-09-27 16:27:15 +0500

    change Task2_0

--- src/Task2_0.java
+++ src/Task2_0.java
@@ -1,4 +1,3 @@
-import java.io.FilterOutputStream;
 import java.util.Scanner;
 
 /*
@@ -16,19 +15,20 @@ public class Task2_0 {
         String jwls = iScaner.next();
         System.out.print("Enter stones: ");
         String stns = iScaner.next();
-        System.out.print(findJewelsInStones(jwls, stns));
+        System.out.println(findJewelsInStones(jwls, stns));
     }
 
     public static String findJewelsInStones(String jewels, String stones) {
         String res = "";
         for (int i = 0; i < jewels.length(); i++) {
             int count = 0;
+            res = res.concat(String.valueOf(jewels.charAt(i)));
             for (int j = 0; j < stones.length(); j++) {
                 if (jewels.charAt(i) == stones.charAt(j)) {
                     count++;
                 }
             }
-            res = res + jewels.charAt(i) + Integer.toString(count);
+            res = res.concat(Integer.toString(count));
         }
         return res;
     }
